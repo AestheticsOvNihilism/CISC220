a3a78164e2a78721522c045a3ec21d8349c27537
AestheticsOvNihilism <chongwangstat@gmail.com>
2016-09-18 16:28:59 -0400

    LAB1Q4

--- Q4.cpp
+++ Q4.cpp
@@ -2,55 +2,60 @@
  * Q4.cpp
  *
  *  Created on: 2016年9月14日
- *      Author: CW
+ *      Author: Chong Wang/ Chu Qiao
  */
+
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
+bool perfectNum(int num);
+int sumsofar(int a, int b, int c);
+
 int main () {
-	int num;
-	cout<<"enter number(an integer from 3 to 29) here:"<<endl;
-	cin>>num;
-/* i ->num
- *
- */
-	int sum=0;
-    for(int i=1; i<30; )
-    {
-    	while (i < num) {
-    			if(num % i == 0){
-    				sum = sum +i;
-    			}
-    			i++;
-    		}
 
-    		if (sum == num){
-    			cout<<num<<" is a perfect number"<<endl;
-    		}
-    		else {
-    			cout<<num<<" is not a perfect number"<<endl;
-    		}
-    		return 0;
-
-    	}
-    }
-
-
-	/*while (i < num) {
-		if(num % i) == 0 {
-			sum = sum +i;
-		}
-		i++;
+	for(int i=1;i<30;i++){
+		cout<<boolalpha;
+	    cout<<i<<" Perfect?"<<perfectNum(i)<<endl;
 	}
-
-	if (sum == num){
-		cout<<num<<"is a perfect number"<<endl;
-	}
-	else {
-		cout<<num<<"is not a perfect number"<<endl;
-	}
-	return 0;
 
 }
-*/
+
+bool perfectNum(int number){
+
+    int a1=number;
+
+    int add = sumsofar(0,a1-1,a1);
+
+    if(number==add){
+
+      return true;
+
+  }//if
+     else{
+        return false;
+ }//else
+}//perfect
+
+int sumsofar(int a, int b, int n){
+    int s;
+    int an;
+    if (b>0){
+        if (n%b==0){
+         s=b;
+       sumsofar(a+s,b-1,n);
+    }//if;
+   else{
+       sumsofar(a,b-1,n);
+      }//else
+    }//if
+   else{
+       an = a;
+     return an;
+   }//else
+}//sum
