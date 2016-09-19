ca71dcf08969e12d99aca41e31cd2ffd486a6fea
AestheticsOvNihilism <chongwangstat@gmail.com>
2016-09-18 20:25:42 -0400

    Q6-12

--- Q6.cpp
+++ Q6.cpp
@@ -20,24 +20,41 @@ using namespace std;
 
 void fillArr(int a[], int ranNum);
 void printArr(int a[], int ranNum);
+//Q7
+
 void reverseArr(int a[], int ranNum);
 void swap(int* a ,int* b);
 void printReversed(int a[], int ranNum);
+//Q8
+
 void findMin(int a[], int size );
+
+//Q10
+void bubblesort(int a[], int ranNum);
+
+//Q11
 
 
 int main(){
+	//Q6
    srand(time(NULL));
    int ranNum=rand() % 30 + 20;
    int a[ranNum];
    fillArr(a, ranNum);
    cout<<"Original array is "<<endl;
    printArr(a, ranNum);
+   //Q7
    cout<<"Reversed array is "<<endl;
    reverseArr(a, ranNum);
    printReversed(a, ranNum);
+   //Q8
    cout<<"min value is "<<endl;
    findMin(a, ranNum);
+
+   //Q11
+   int filter=rand() % 5 + 3;
+   windowsize(filter);
+
 }
 
 void fillArr(int* a, int c){
@@ -101,3 +118,41 @@ void sumArray(int*a, int index, int ranNum){
 	for(i=0,)
 }
 */
+
+/* q10
+void bubblesort(int* a, int b){
+	int counter, avg = 0;
+
+}
+*/
+
+// Q11
+int windowsize(int filter){
+       int filter_size= *filter%2;
+       int windowsize;
+       if (filter_size==0){
+    	   windowsize=*filter + 1;
+       }
+       else {
+    	   windowsize= *filter;
+       }
+       return windowsize;
+}
+
+int startLoc(int filter){
+	int start_index = filter%2 - 1;
+	return start_index;
+}
+
+
+
+/*
+int endLoc(int filter, int ranNum2){
+	int end_index = ranNum2 - 1- filter%2 ;
+	return end_index;
+}
+*/
+
+//Q12
+
+
